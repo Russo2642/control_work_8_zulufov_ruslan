b47c4c87679a1e54c3db9374fb528ecf92542adb
Ruslan Zulufov <gothiq2302@gmail.com>
2023-03-25 13:11:42 +0600

    Добавил CRUD операции с товарами

--- reviewer/products/urls.py
+++ reviewer/products/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from products.views.base import IndexView
 
-from products.views.products import ProductCreateView, ProductDetailView
+from products.views.products import ProductCreateView, ProductDetailView, ProductUpdateView, ProductDeleteView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('product/create', ProductCreateView.as_view(), name='product_create'),
     path('product/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
+    path('product/<int:pk>/update', ProductUpdateView.as_view(), name='product_update'),
+    path('product/<int:pk>/delete', ProductDeleteView.as_view(), name='product_delete'),
 ]
--- reviewer/products/views/products.py
+++ reviewer/products/views/products.py
@@ -1,6 +1,6 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse
-from django.views.generic import CreateView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from products.forms import ProductForm
 from products.models import Product
@@ -18,3 +18,20 @@ class ProductCreateView(LoginRequiredMixin, CreateView):
 class ProductDetailView(DetailView):
     template_name = 'products/product_detail.html'
     model = Product
+
+
+class ProductUpdateView(UpdateView):
+    template_name = 'products/product_update.html'
+    model = Product
+    form_class = ProductForm
+
+    def get_success_url(self):
+        return reverse('product_detail', kwargs={'pk': self.object.pk})
+
+    # def test_func(self):
+    #     return self.request.user.has_perm('product.change_product')
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('index')
